a47eca3a68b005070afac54403dcfd2875c50a91
jonah0502 <jonah0502@gmail.com>
2021-05-15 17:03:01 -0700

    updated server functionality

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from "cors"
+import movies from "./api/movies.route.js"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json()) // allows server to accept json requests
+app.use("/api/v1/movies", movies)
+
+app.use("*", (req, res)=> res.status(404).json({error: "page not found"}))
+export default app
