5eaf6004b1f9222e17702f20ae81270c7c2dd0c5
efmmoncada <emmanuel.moncada27@gmail.com>
2022-03-04 16:48:47 -0800

    fix imbd ratings

--- frontend/public/addmovie_scripts.js
+++ frontend/public/addmovie_scripts.js
@@ -1,57 +1,66 @@
-var admovie = document.getElementById('submit')
-var master_genre_list = "Action Adventure Animation Biography Comedy Crime Documentary Drama Family Fantasy Film-Noir History Horror Music Musical Mystery News Romance Sci-Fi Short Sport Talk-Show Thriller War Western"
+var addMovie = document.getElementById("submit");
+var master_genre_list =
+  "Action Adventure Animation Biography Comedy Crime Documentary Drama Family Fantasy Film-Noir History Horror Music Musical Mystery News Romance Sci-Fi Short Sport Talk-Show Thriller War Western";
 
-admovie.addEventListener('click', () =>{
-    var plot1 = document.getElementById('plot-input').value.toString()
-    var title1 = document.getElementById('title-input').value.toString()
-    var year1 = document.getElementById('year-input').value.toString()
-    var director1 = document.getElementById('director-input').value.toString()
-    var image1 = document.getElementById('image-link').value.toString()
-    var rating1 = document.getElementById('rating-input').value.toString()
-    var genre1 = document.getElementById('genre-input').value.toString()
-    var tag1 = document.getElementById('tag-input').value.toString()
+addMovie.addEventListener("click", () => {
+  var plot1 = document.getElementById("plot-input").value.toString();
+  var title1 = document.getElementById("title-input").value.toString();
+  var year1 = document.getElementById("year-input").value.toString();
+  var director1 = document.getElementById("director-input").value.toString();
+  var image1 = document.getElementById("image-link").value.toString();
+  var rating1 = document.getElementById("rating-input").value.toString();
+  var genre1 = document.getElementById("genre-input").value.toString();
+  var tag1 = document.getElementById("tag-input").value.toString();
 
-    if(plot1 && title1 && year1 && director1 && image1 && rating1 && genre1 && tag1) //checks for all input boxes to have infomation in them
-    {
-      var request = new XMLHttpRequest()
-      request.open('POST', 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movie-new')
-      request.setRequestHeader('Content-Type', 'application/json')
+  if (
+    plot1 &&
+    title1 &&
+    year1 &&
+    director1 &&
+    image1 &&
+    rating1 &&
+    genre1 &&
+    tag1
+  ) {
+    //checks for all input boxes to have infomation in them
+    var request = new XMLHttpRequest();
+    request.open(
+      "POST",
+      "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movie-new"
+    );
+    request.setRequestHeader("Content-Type", "application/json");
 
-      var director_list = director1.split(",");//splits director names at commas
-      var genre_list = genre1.split(" ");//splits genre at the space between each one
-      var tag_list = tag1.split(" ");//splits tags at the space between each one
+    var director_list = director1.split(","); //splits director names at commas
+    var genre_list = genre1.split(" "); //splits genre at the space between each one
+    var tag_list = tag1.split(" "); //splits tags at the space between each one
 
-      var flag = 0;
-      for(var i = 0; i < genre_list.length;i++)
-      {
-        if(master_genre_list.search(genre_list[i]) == -1)
-        {
-            flag = 1
-            alert("Invalid genre detected, please refer to genre dropdown above for valid options")
-            break;
-        }
-      }
-  
-      if(flag == 0)
-      {
-        var newmovie  = {
-          title: title1.toString(),
-          plot: plot1.toString(),
-          genres: genre_list,
-          year: parseInt(year1),
-          poster: image1.toString(),
-          tags: tag_list,
-          directors: director_list,
-          rated: rating1.toString(),
-        }
-        var requestBody = JSON.stringify(newmovie) //packages as a json and sends to API
-        request.send(requestBody)
-        location.href = '/'
-       
+    var flag = 0;
+    for (var i = 0; i < genre_list.length; i++) {
+      if (master_genre_list.search(genre_list[i]) == -1) {
+        flag = 1;
+        alert(
+          "Invalid genre detected, please refer to genre dropdown above for valid options"
+        );
+        break;
       }
     }
-    else {
-      alert("Not all infomation has been filled in");
-    }
 
-})
+    if (flag == 0) {
+      var newmovie = {
+        title: title1.toString(),
+        plot: plot1.toString(),
+        genres: genre_list,
+        year: parseInt(year1),
+        poster: image1.toString(),
+        tags: tag_list,
+        directors: director_list,
+        rated: rating1.toString(),
+      };
+      var requestBody = JSON.stringify(newmovie); //packages as a json and sends to API
+      request.send(requestBody);
+      location.href = "/";
+    }
+  } else {
+    alert("Not all infomation has been filled in");
+  }
+});
--- frontend/server.js
+++ frontend/server.js
@@ -1,281 +1,301 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const axios = require('axios')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const axios = require("axios");
 
-const app = express()
+const app = express();
 
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}))
-app.set('view engine', 'handlebars')
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
 
+app.get("/", (req, res) => {
+  res.redirect("/page/1");
+});
 
-app.get('/', (req, res) => {
-  res.redirect('/page/1')
-})
+app.get("/page/:num", (req, res) => {
+  if (parseInt(req.params.num) < 1) req.params.num = 1;
+  var queryNum = parseInt(req.params.num) - 1;
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?page=" +
+    queryNum.toString();
+  axios.get(url).then((data) => {
+    var movieArray = data.data.movies;
 
-
-app.get('/page/:num', (req, res) => {
-  if (parseInt(req.params.num) < 1) req.params.num = 1
-  var queryNum = parseInt(req.params.num) - 1
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?page=' + queryNum.toString()
-  axios.get(url).then(data => {
-    var movieArray = data.data.movies
-
-    var homeContext = []
+    var homeContext = [];
     for (var i = 0; i < movieArray.length; i++) {
-      if (!movieArray[i].poster) continue
-      var title = movieArray[i].title
-      if (title.length > 25) title = title.slice(0, 22) + ' ...'
+      if (!movieArray[i].poster) continue;
+      var title = movieArray[i].title;
+      if (title.length > 25) title = title.slice(0, 22) + " ...";
       var movieObj = {
         title: title,
         moviePageURL: "/movies/" + movieArray[i]._id,
         movieCoverURL: movieArray[i].poster,
-      }
-      homeContext.push(movieObj)
-      if (homeContext.length >= 21) break
+      };
+      homeContext.push(movieObj);
+      if (homeContext.length >= 21) break;
     }
-    res.status(200).render('home', {
+    res.status(200).render("home", {
       movies: homeContext,
-      page: (Number(req.params.num))
-    })
-  })
-})
+      page: Number(req.params.num),
+    });
+  });
+});
 
 // These functions were adapted from this post: https://stackoverflow.com/questions/52669596/promise-all-with-axios/52669775
-function getAllData(tagData){
+function getAllData(tagData) {
   return Promise.all(tagData.map(fetchData));
 }
 
 function fetchData(tagData) {
   return axios
     .get(tagData.url)
-    .then(function(response) {
-      movieArray = response.data.movies
-      return movieObj = {
+    .then(function (response) {
+      movieArray = response.data.movies;
+      return (movieObj = {
         title: tagData.tag,
         moviePageURL: "/tags/" + tagData.tag,
-        movieCoverURL: movieArray[0].poster
-      }
+        movieCoverURL: movieArray[0].poster,
+      });
     })
-    .catch(e => console.log(`error creating movie objets: ${e}`));
+    .catch((e) => console.log(`error creating movie objets: ${e}`));
 }
 
-app.get('/tagPage', (req, res) => { //pulls all tags and displays on page
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/tags'
-   axios.get(url).then(async response => {
-    var homeContext
-    var tagData = []
-    tagsArray = response.data;
-    tagsArray.forEach((tag) => {
-      const tagStuff = {
-      url: 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?tag=' + tag,
-      tag: tag
-      }
-      tagData.push(tagStuff)
+app.get("/tagPage", (req, res) => {
+  //pulls all tags and displays on page
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/tags";
+  axios
+    .get(url)
+    .then(async (response) => {
+      var homeContext;
+      var tagData = [];
+      tagsArray = response.data;
+      tagsArray.forEach((tag) => {
+        const tagStuff = {
+          url:
+            "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?tag=" +
+            tag,
+          tag: tag,
+        };
+        tagData.push(tagStuff);
+      });
+      homeContext = await getAllData(tagData);
+
+      res.status(200).render("tags", {
+        movies: homeContext,
+        page: Number(req.params.num),
+      });
     })
-    homeContext = await getAllData(tagData)
+    .catch((e) => console.log(`error getting tags: ${e}`));
+});
 
-    res.status(200).render('tags', {
-      movies: homeContext,
-      page: (Number(req.params.num))
-    })
-  }).catch(e => console.log(`error getting tags: ${e}`));
-})
+app.get("/titles/:title", (req, res) => {
+  var que = '"' + req.params.title + '"';
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?title=" +
+    que;
+  axios.get(url).then((data) => {
+    var movieArray = data.data.movies;
 
-
-app.get('/titles/:title', (req, res) => {
-  var que = '"' + req.params.title + '"'
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?title=' + que
-  axios.get(url).then(data => {
-    var movieArray = data.data.movies
-
-    var homeContext = []
+    var homeContext = [];
     for (var i = 0; i < movieArray.length; i++) {
-      if (movieArray[i].title.search(que) != -1 && movieArray[i].poster != null) continue
+      if (movieArray[i].title.search(que) != -1 && movieArray[i].poster != null)
+        continue;
       var movieObj = {
         title: movieArray[i].title,
         moviePageURL: "/movies/" + movieArray[i]._id,
         movieCoverURL: movieArray[i].poster,
-      }
-      homeContext.push(movieObj)
-      if (homeContext.length >= 21) break
+      };
+      homeContext.push(movieObj);
+      if (homeContext.length >= 21) break;
     }
     if (homeContext.length == 0) {
-      res.status(200).render('no_results')
+      res.status(200).render("no_results");
     } else {
-      res.status(200).render('home', {
-        movies: homeContext
-      })
+      res.status(200).render("home", {
+        movies: homeContext,
+      });
     }
-  })
-})
+  });
+});
 
-app.get('/IMDB/:rating', (req, res) => {
-  var que = parseInt(req.params.rating)
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?IMDB=' + que.toString()
-  axios.get(url).then(data => {
-    var movieArray = data.data.movies
-    var homeContext = []
+app.get("/IMDB/:rating", (req, res) => {
+  var que = parseInt(req.params.rating);
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?IMDB=" +
+    que.toString();
+  axios.get(url).then((data) => {
+    var movieArray = data.data.movies;
+    var homeContext = [];
     for (var i = 0; i < movieArray.length; i++) {
-      if (movieArray[i].rating >= que && movieArray[i].poster != null) continue
+      if (movieArray[i].rating >= que && movieArray[i].poster != null) continue;
       var movieObj = {
         title: movieArray[i].title,
         moviePageURL: "/movies/" + movieArray[i]._id,
         movieCoverURL: movieArray[i].poster,
-      }
-      homeContext.push(movieObj)
-      if (homeContext.length >= 21) break
+      };
+      homeContext.push(movieObj);
+      if (homeContext.length >= 21) break;
     }
     if (homeContext.length == 0) {
-      res.status(200).render('no_results')
+      res.status(200).render("no_results");
     } else {
-      res.status(200).render('home', {
-        movies: homeContext
-      })
+      res.status(200).render("home", {
+        movies: homeContext,
+      });
     }
-  })
-})
+  });
+});
 
-app.get('/Year_Range/:years', (req, res) => {
-  var que = req.params.years
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?year_range=' + que
+app.get("/Year_Range/:years", (req, res) => {
+  var que = req.params.years;
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?year_range=" +
+    que;
   //var values = que.split("_")
-  axios.get(url).then(data => {
-    var movieArray = data.data.movies
-    var homeContext = []
+  axios.get(url).then((data) => {
+    var movieArray = data.data.movies;
+    var homeContext = [];
     for (var i = 0; i < movieArray.length; i++) {
-        var movieObj = {
-          title: movieArray[i].title,
-          moviePageURL: "/movies/" + movieArray[i]._id,
-          movieCoverURL: movieArray[i].poster,
-        }
-      homeContext.push(movieObj)
-      if (homeContext.length >= 21) break
+      var movieObj = {
+        title: movieArray[i].title,
+        moviePageURL: "/movies/" + movieArray[i]._id,
+        movieCoverURL: movieArray[i].poster,
+      };
+      homeContext.push(movieObj);
+      if (homeContext.length >= 21) break;
     }
     if (homeContext.length == 0) {
-      res.status(200).render('no_results')
+      res.status(200).render("no_results");
     } else {
-      res.status(200).render('home', {
-        movies: homeContext
-      })
+      res.status(200).render("home", {
+        movies: homeContext,
+      });
     }
-  })
-})
+  });
+});
 
-
-app.get('/genres/:genre', (req, res) => {
-  var que = req.params.genre
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?genre=' + que
-  axios.get(url).then(data => {
-    var movieArray = data.data.movies
-    que.toString()
-    var homeContext = []
+app.get("/genres/:genre", (req, res) => {
+  var que = req.params.genre;
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?genre=" +
+    que;
+  axios.get(url).then((data) => {
+    var movieArray = data.data.movies;
+    que.toString();
+    var homeContext = [];
     for (var i = 0; i < movieArray.length; i++) {
-      var temp = movieArray[i].genres
-      for(var k = 0; k < temp.length; k++){
-        if (que.search(temp[k]) != -1 && movieArray[i].poster != null){
+      var temp = movieArray[i].genres;
+      for (var k = 0; k < temp.length; k++) {
+        if (que.search(temp[k]) != -1 && movieArray[i].poster != null) {
           var movieObj = {
             title: movieArray[i].title,
             moviePageURL: "/movies/" + movieArray[i]._id,
             movieCoverURL: movieArray[i].poster,
-          }
-          homeContext.push(movieObj)
+          };
+          homeContext.push(movieObj);
           break;
         }
+      }
+      if (homeContext.length >= 21) break;
     }
-    if (homeContext.length >= 21) break
-      }
-      if(homeContext.length == 0)
-      {
-        res.status(200).render('no_results')
-      }
-      else{
-        res.status(200).render('home', {
-        movies: homeContext
-      })
-      }
-  })
-})
+    if (homeContext.length == 0) {
+      res.status(200).render("no_results");
+    } else {
+      res.status(200).render("home", {
+        movies: homeContext,
+      });
+    }
+  });
+});
 
-
-app.get('/tags/:tag', (req, res) => {
-  var que = req.params.tag
+app.get("/tags/:tag", (req, res) => {
+  var que = req.params.tag;
   if (que.toString().length == 0) {
-    res.status(404).render('404pg')
+    res.status(404).render("404pg");
   }
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?tag=' + que
-  axios.get(url).then(data => {
-    var movieArray = data.data.movies
-    que.toString()
-    var homeContext = []
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movies?tag=" +
+    que;
+  axios.get(url).then((data) => {
+    var movieArray = data.data.movies;
+    que.toString();
+    var homeContext = [];
     for (var i = 0; i < movieArray.length; i++) {
-      var temp = movieArray[i].tags
+      var temp = movieArray[i].tags;
       for (var k = 0; k < temp.length; k++) {
         if (que.search(temp[k]) != -1) {
           var movieObj = {
             title: movieArray[i].title,
             moviePageURL: "/movies/" + movieArray[i]._id,
             movieCoverURL: movieArray[i].poster,
-          }
-          homeContext.push(movieObj)
-          break
+          };
+          homeContext.push(movieObj);
+          break;
         }
       }
-      if (homeContext.length >= 21) break
+      if (homeContext.length >= 21) break;
     }
-
 
     if (homeContext.length == 0) {
-      res.status(200).render('no_results', {scripts: ["/index.js"]})
+      res.status(200).render("no_results", { scripts: ["/index.js"] });
     } else {
-      res.status(200).render('home', {
-        movies: homeContext
-      })
+      res.status(200).render("home", {
+        movies: homeContext,
+      });
     }
-  })
-})
+  });
+});
 
-
-
-app.get('/movies/:id', (req, res) => {
-  var movieId = req.params.id
-  const url = 'https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movie?id=' + movieId
-  axios.get(url).then(data => {
+app.get("/movies/:id", (req, res) => {
+  var movieId = req.params.id;
+  const url =
+    "https://us-west-2.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/tagflix-gprxr/service/movies/incoming_webhook/movie?id=" +
+    movieId;
+  axios.get(url).then((data) => {
     var movieObj = {
       posterUrl: data.data.poster,
       rated: data.data.rated,
-      imdb: data.data.imdb.rating,
+      imdb: data.data.imdb.rating.$numberDouble,
       title: data.data.title,
       director: data.data.directors,
       year: data.data.year,
       fullPlot: data.data.fullplot,
-      genres: data.data.genres.join(', '),
-      tags: data.data.tags.join(', '),
-      styles: ['/movie_style.css'],
-      scripts: ['https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.runtime.js', '/reviewTemplate.js', '/movie_scripts.js'],
-      reviews: data.data.reviews
-    }
+      genres: data.data.genres.join(", "),
+      tags: data.data.tags.join(", "),
+      styles: ["/movie_style.css"],
+      scripts: [
+        "https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.runtime.js",
+        "/reviewTemplate.js",
+        "/movie_scripts.js",
+      ],
+      reviews: data.data.reviews,
+    };
 
-    res.status(200).render('moviePage', movieObj)
-  })
-})
+    res.status(200).render("moviePage", movieObj);
+  });
+});
 
-app.get('/add-movie/', (req, res) => {
+app.get("/add-movie/", (req, res) => {
+  var scripts = [
+    "https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.runtime.js",
+    "/addmovie_scripts.js",
+  ];
+  res.status(200).render("addmoviepg", { scripts });
+});
 
-  var scripts =  ['https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.runtime.js', '/addmovie_scripts.js']
-  res.status(200).render('addmoviepg', {scripts} )
+app.use(express.static("./public"));
 
-})
+app.get("*", (req, res) => {
+  res.status(404).render("404pg");
+});
 
-app.use(express.static("./public"))
-
-app.get('*', (req, res) => {
-  res.status(404).render('404pg')
-})
-
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log('== Listening on port', port);
-})
+  console.log("== Listening on port", port);
+});
